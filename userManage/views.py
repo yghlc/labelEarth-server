07755c617a9ce1136990572d69296360890cd2ca
Lingcao Huang <huanglingcao@gmail.com>
2022-04-26 21:13:06 -0600

    change link

--- userManage/views.py
+++ userManage/views.py
@@ -40,7 +40,7 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                return redirect("homepage")
+                return redirect("/visual/index.html?username=%s"%username)
             else:
                 messages.error(request, "Invalid username or password.")
         else:
